cb813b2c1eac9e373e4da65f2e0fef8c0ecd6675
Harry Sanders <harry.sandersiv@yahoo.com>
2026-03-03 18:27:33 -0500

    app init

---
+++ app/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import { Product } from '../components/Product';
+
+export function Home() {
+  return (
+    <>
+      <div>
+        <h1>Home!!</h1>
+        <div>
+          <Product />
+        </div>
+      </div>
+    </>
+  );
+}
