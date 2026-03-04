6b6250974e72156c1f1446767a347fc7186128fd
Harry Sanders <harry.sandersiv@yahoo.com>
2026-03-03 19:02:08 -0500

    html

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import { Home } from './pages/Home';
+import './App.css';
+
+export function App() {
+  return (
+    <>
+      <div>
+        <h1>hello world</h1>
+        <Home />
+      </div>
+    </>
+  );
+}
---
+++ app/src/components/Product.tsx
@@ -0,0 +1,10 @@
+export function Product() {
+  return (
+    <>
+      <h3>Title</h3>
+      <img></img>
+      <h4>description</h4>
+      <h4>Price</h4>
+    </>
+  );
+}
