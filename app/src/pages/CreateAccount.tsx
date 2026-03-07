84b937b2b62b675a32f40f021308d60fbb773eb8
James <james_rowlands@daytonastate.edu>
2026-03-07 11:48:25 -0500

    Created css for login, product and createaccount pages

--- app/src/pages/CreateAccount.tsx
+++ app/src/pages/CreateAccount.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
+import './CreateAccount.css'
 export function CreateAccount() {
   const [formData, setFormData] = useState({
     firstName: '',
@@ -22,22 +22,22 @@ export function CreateAccount() {
   };
 
   return (
-    <div className="form-container">
-      <h2>Create Account</h2>
+    <div className="accountcontainer">
+      <h2 className='textcolor'>Create Account</h2>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="First Name" required
+        <input className='textbox' type="text" placeholder="First Name" required
           onChange={(e) => setFormData({...formData, firstName: e.target.value})} />
-        
-        <input type="text" placeholder="Last Name" required
+        <br></br>
+        <input className='textbox' type="text" placeholder="Last Name" required
           onChange={(e) => setFormData({...formData, lastName: e.target.value})} />
-        
-        <input type="email" placeholder="Email" required
+        <br></br>
+        <input className='textbox' type="email" placeholder="Email" required
           onChange={(e) => setFormData({...formData, email: e.target.value})} />
-        
-        <input type="password" placeholder="Password" required
+        <br></br>
+        <input className='textbox' type="password" placeholder="Password" required
           onChange={(e) => setFormData({...formData, password: e.target.value})} />
-        
-        <button type="submit">Register</button>
+        <br></br><br></br>
+        <button className='button' type="submit">Register</button>
       </form>
     </div>
   );
--- app/src/pages/Login.tsx
+++ app/src/pages/Login.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
+import './Login.css'
 export function Login() {
   const [loginData, setLoginData] = useState({ email: '', password: '' });
   const navigate = useNavigate();
-
+  const handleCreate = ()=> {
+    navigate('/createaccount')
+  };
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
@@ -19,16 +21,18 @@ export function Login() {
   };
 
   return (
-    <div className="form-container">
-      <h2>Login</h2>
+    <div className="logincontainer">
+      <h2 className="textcolor">Login</h2>
       <form onSubmit={handleLogin}>
-        <input type="email" placeholder="Email" required
+        <input className='textbox' type="email" placeholder="Email" required
           onChange={(e) => setLoginData({...loginData, email: e.target.value})} />
-        
-        <input type="password" placeholder="Password" required
+        <br></br>
+        <input className='textbox' type="password" placeholder="Password" required
           onChange={(e) => setLoginData({...loginData, password: e.target.value})} />
-        
-        <button type="submit">Login</button>
+        <br></br>  <br></br>
+        <button className='button' type="submit">Login</button>
+         <br></br>
+        <a className='create' onClick={handleCreate}>Create Account</a>
       </form>
     </div>
   );
